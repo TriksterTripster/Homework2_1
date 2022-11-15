41c55e21c5abdba268a51e5f8996c8cd8e6c4687
TriksterTripster <1387751@mail.ru>
2022-11-15 23:16:39 +0500

    init commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int price = 14_549;
+
+        int miles = price / 20;
+
+        System.out.println("Бонусных миль:" + miles);
+
+
+    }
+
+}
